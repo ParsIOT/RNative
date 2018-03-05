8ad2e9a6b41e0980643374344b75da02978cf80c
kjj <j.keyvance@gmail.com>
2018-03-05 15:48:16 +0330

    ios

---
+++ index2.ios.js
@@ -0,0 +1,29 @@
+import {StackNavigator} from 'react-navigation';
+import {AppRegistry} from 'react-native'
+
+// import beacon from './app/screens/beacons-page'
+// AppRegistry.registerComponent('reactNativeBeaconExample', () => beacon);
+
+import App from './app/index'
+
+// import MessageQueue from 'react-native/Libraries/BatchedBridge/MessageQueue';
+// MessageQueue.spy(true);
+
+AppRegistry.registerComponent('reactNativeBeaconExample', () => App);
+
+
+// import React, { Component } from 'react';
+// import { WebView ,AppRegistry} from 'react-native';
+
+// class MyWeb extends Component {
+//   render() {
+//     return (
+//       <WebView
+// source={require('./home.html')}
+//         style={{marginTop: 20}}
+//       />
+//     );
+//   }
+// }
+
+//  AppRegistry.registerComponent('reactNativeBeaconExample', ()=> MyWeb);
